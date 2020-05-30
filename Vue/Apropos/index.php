527296b21128d65e4c0c719b5527e4ef35b68bb6
Marwane Rezgui <clibeau2010@hotmail.com>
2020-05-30 16:38:21 -0400

    apropos et lien git finit

--- Vue/Apropos/index.php
+++ Vue/Apropos/index.php
@@ -1,6 +1,7 @@
 <?php $titre = 'Le Blogue du prof version 1.0.0.1'; ?>
 
 <ul>
+<h2><a href="https://github.com/marbawws/WEBTP5.git">LIEN VERS LE DEPOT GIT</a></h2>
     <li>Marwane Rezgui</li>
 
     <li>420-4A4 MO Web et Bases de données</li>
@@ -18,67 +19,48 @@
         </li>
         <li>
             Pour fin de démonstration, cette version offre la possibilité de changer de contrôleur d'accueil.<br/>
-            L'accueil présente alors plutôt la liste de tous les commentaires à l'accueil :
-            <ul>
-                <li>
-                    Chaque commentaire indique alors le titre de l'article pour lequel il a été écrit, avec un lien vers cet article.
-                </li>
-                <li>
-                    Cela peut vous être utile si vous désirez présenter à l'accueil le côté n de la relation 1 à n pour votre application.
-                </li>
+            L'accueil présente alors plutôt la liste de tous les places à l'accueil :
+			L'accueil présente alors plutôt la liste de tous les places à l'accueil, avec un lien menant à l'transaction associé.<br/>
+            Cela peut vous être utile si vous désirez présenter à l'accueil le côté n de la relation 1 à n pour votre application.
             </ul>
         </li>
         <li>
-            <form action="<?= $utilisateur != '' ? 'Admin' : ''; ?>commentaires" method="post">
+            <form action="<?= $utilisateur != '' ? 'Admin' : ''; ?>places" method="post">
                 <input type="submit" value="Changer de controleur d'accueil">
             </form>
         </li>
     </ul>
     <li>Si un utilisateur est en session : </li>
     <ul>
-        <li>on retrouve un lien pour créer un nouvel article :
+        <li>On y retrouve un lien pour créer une nouvelle transaction :
             <ul>
                 <li>
-                    L'article créé est attribué à l'utilisateur en session
-                </li>
-                <li>
-                    La page de création d'un article offre de spécifier le sujet traité par
-                    l'article (type) par autocomplétion.<br>(par http seulement ; ne fonctionne pas avec https pour l'instant)
+                    La transaction créé est attribué à l'utilisateur en session
                 </li>
             </ul>
         </li>
         <li>
-            Les actions pour effacer/rétablir un commentaire sont affichées ;
+            Les actions pour effacer/rétablir une place sont affichées ;
         </li>
         <li>
-            Les commentaires privés sont affichés ;
-        </li>
-        <li>
-            Il n'est plus possible d'ajouter un commentaire pour un article.
+            Il n'est plus possible d'ajouter une place pour une transaction.
         </li>
     </ul>
-    <li>Les lecteurs du blogue peuvent cliquer sur le titre d'un
-        article pour lire le texte complet de l'article :<br>
-    </li>
     <ul>
-        <li>À la suite du texte de l'article on offre la possibilité de
-            laisser un commentaire sur l'article ;</li>
-        <li>La personne qui veut laisser un commentaire doit
+        <li>À la suite du texte de la transaction on offre la possibilité de
+            laisser une place sur la transaction ;</li>
+        <li>La personne qui veut laisser une place doit
             s'identifier à l'aide d'un courriel valide :</li>
         <ul>
-            <li>Un message est affiché si le courriel est invalide et le
-                commentaire n'est pas enregistré.<br>
+            <li>Un message est affiché si le courriel est invalide et la
+                place n'est pas enregistré.<br>
             </li>
         </ul>
-        <li>On peut spécifier s'il s'agit d'un commentaire privé destiné
-            seulement à l'auteur de l'article :</li>
         <ul>
-            <li>Pour l'instant cette fonctionnalité n'est pas encore
-                implantée et tous les commentaires sont affichés.</li>
         </ul>
-        <li>On peut effacer un commentaire après confirmation (par l'auteur de l'article dans une prochaine version).</li>
-        <li>Un commentaire effacé peut être rétabli (par l'auteur de l'article dans une prochaine version).</li>
-        <li>Un commentaire ne peut pas être modifié.<br>
+        <li>On peut effacer une place après confirmation (par l'auteur de la transaction dans une prochaine version).</li>
+        <li>Une place effacé peut être rétabli (par l'auteur de la transaction dans une prochaine version).</li>
+        <li>Une place ne peut pas être modifié.<br>
         </li>
     </ul>
 </ul>
@@ -88,17 +70,17 @@
     <tr>
         <td>
             <h4>Base de données utilisée par l'application :</h4>
-            <object data="Contenu/images/Blogue-vers-MVC-v1_1_0_1.svg" type="image/svg+xml" height="500" width="800">
-                <img src="Contenu/images/blogue_phpmyadmin.jpg" alt=""/>
+            <object data="Contenu/images/Blogue-vers-MVC-v1_0_0_0.svg" type="image/svg+xml" height="500" width="800">
+                <img src="Contenu/images/maBD.PNG" alt=""/>
             </object><br/>
         </td>
     </tr>
     <tr>
         <td>
             <h4>Basé sur ce modèle original :</h4>
-            <a href="http://www.databaseanswers.org/data_models/big_data_hadoop/index.htm" >
-                <img src="Contenu/images/data_model.gif" alt=""/><br/>
-                Blog as a "Real Life example :</a>
+            <a href="http://www.databaseanswers.org/data_models/fuel_stock/index.htm" >
+                <img src="Contenu/images/bd_Barry.png" alt=""/><br/>
+                Lien vers la bd :</a>
         </td>
     </tr>
 </table>
